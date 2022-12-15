d08e2da12e6260adea93d0bd500332ae5e74673b
Kefren <kefrenkennedyav@gmail.com>
2022-12-15 09:33:08 -0300

    feature: created the base of the globalStyle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React, { Fragment } from 'react';
+import GlobalStyle from './theme/globalStyle';
+import Content from './components/Content';
+
 function App() {
   return (
-    <div>
-    </div>
+    <Fragment>
+      <GlobalStyle />
+      <Content />    {/* <Content /> is a component that contains all the other components for the application. */}
+    </Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { ThemeProvider } from 'styled-components'
+
+import { App } from './App'
+import { theme } from './theme'
+import GlobalCSS from './global.css'
+
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <GlobalCSS />
     <App />
-  </React.StrictMode>
-);
+  </ThemeProvider>, document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
