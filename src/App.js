6b6ef8e68ff8d78e4776f403f0e0a3e539595ba7
Fabiojcp <fabio50019@gmail.com>
2022-12-16 16:46:19 -0300

    feat: style provider created and called in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import './style/styleGuide.css'
+import './style/styleGuide.css';
 import './style/resetCSS.css';
+import { StyleProvider } from './provider/styleProvider';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <StyleProvider>
+      </StyleProvider>
+    </BrowserRouter>
   );
 }
 
