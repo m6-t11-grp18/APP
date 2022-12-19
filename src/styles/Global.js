d34e6104e540af6d1147bb0e243f8c8d8472489e
Matheus de Souza Arruda <kahlis.dev@gmail.com>
2022-12-19 06:36:53 -0300

    feat: added global style

---
+++ src/styles/Global.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    button{
+        cursor: pointer;
+    }
+
+    ul, ol, li {
+        list-style: none;
+    }
+
+    body {
+        background-color: #121214;
+    }
+`;
